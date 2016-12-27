20937a71e41108ef61111d9bab6d26ee7eef0c34
Brian Caldwell <caldwerl@gmail.com>
2016-12-27 10:45:01 -0500

    Add first unit files

---
+++ 1-drumkit/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var key;
+var audio;
+var keys = Array.from(document.querySelectorAll('.key'));
+
+function removeTransition(e) {
+
+    if (e.propertyName !== 'transform') {
+        return;
+    }
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+
+    audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+    key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+
+    if (!audio) {
+        return;
+    }
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
